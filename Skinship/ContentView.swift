d98c2e10cd960c031ee2c15500ab02b793dff98d
RESOANA <quantumjump1103@icloud.com>
2025-07-20 13:58:40 +0900

    Initial Commit

---
+++ Skinship/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Skinship
+//
+//  Created by 田中　よしき on 2025/07/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Skinship/SkinshipApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkinshipApp.swift
+//  Skinship
+//
+//  Created by 田中　よしき on 2025/07/20.
+//
+
+import SwiftUI
+
+@main
+struct SkinshipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
